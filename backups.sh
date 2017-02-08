45bbb32a14b694f821ffcc3b7ffe9dd56057bda9
Mathias Van de Pol <mathyke007@gmail.com>
2017-02-08 14:23:10 +0100

    Update backups.sh

--- backups.sh
+++ backups.sh
@@ -5,28 +5,30 @@ source /mongodb_env.sh
 MYDATE=`date +%Y-%m-%d`
 MONTH=$(date +%m)
 YEAR=$(date +%Y)
+HOUR=$(date +%H)
+MINUTE=$(date +%M)
 MYBASEDIR=/backups
-MYBACKUPDIR=${MYBASEDIR}/${YEAR}/${MONTH}
+MYBACKUPDIR=${MYBASEDIR}/${YEAR}/${MONTH}/${HOUR}${MINUTE}
 mkdir -p ${MYBACKUPDIR}
 cd ${MYBACKUPDIR}
+ls -al
 
 echo "[MONGO_BACKUP] Backup running to $MYBACKUPDIR" >> /var/log/cron.log
 echo "[MONGO_BACKUP] Backing up DB $MONGODB_DATABASE_FROM on host $MONGODB_HOST"  >> /var/log/cron.log
 
-DUMP_OUTPUT="$(mongodump -h $MONGODB_HOST -d $MONGODB_DATABASE_FROM 2>&1)"
-
+mongodump -h $MONGODB_HOST -d $MONGODB_DATABASE_FROM >> /var/log/cron.log
 if [ $? -ne 0 ]; then
-  echo "[MONGO_BACKUP] Error: Failed to backup $MONGODB_DATABASE_FROM. Error was: $DUMP_OUTPUT" >> /var/log/cron.log
+  echo "[MONGO_BACKUP] Error: Failed to backup $MONGODB_DATABASE_FROM"
   exit 0
 else
-  echo "[MONGO_COPY] Copying DB $MONGODB_DATABASE_FROM on host $MONGODB_HOST to $MONGODB_DATABASE_TO"  >> /var/log/cron.log   
-  COPY_OUTPUT="$(mongorestore -h MONGODB_HOST -d MONGODB_DATABASE_TO dump)"
+  echo "[MONGO_COPY] Copying DB $MONGODB_DATABASE_FROM on host $MONGODB_HOST to $MONGODB_DATABASE_TO"  >> /var/log/cron.log
+  mongorestore -h $MONGODB_HOST -d $MONGODB_DATABASE_TO dump/${MONGODB_DATABASE_FROM} >> /var/log/cron.log
   if [ $? -ne 0 ]; then
-    echo "[MONGO_COPY] Error: Failed to copy $MONGODB_DATABASE_FROM. to  $MONGODB_DATABASE_TO Error was: $COPY_OUTPUT" >> /var/log/cron.log
+    echo "[MONGO_COPY] Error: Failed to copy $MONGODB_DATABASE_FROM to $MONGODB_DATABASE_TO"
     exit 0
   fi
 fi
 
-FILENAME=${MYBACKUPDIR}/${MONGODB_DATABASE}.${MYDATE}.dump.tgz
+FILENAME=${MYBACKUPDIR}/${MONGODB_DATABASE_FROM}.${MYDATE}.dump.tgz
 tar -zcvf $FILENAME dump
 rm -rf dump
